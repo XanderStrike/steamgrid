59f7310a9b199525edfbd3d28e5d78c823dcbe79
Alexander Standke <xanderstrike@gmail.com>
2014-03-24 09:59:31 -0700

    add form

--- server.rb
+++ server.rb
@@ -10,10 +10,10 @@ require 'json'
 set :bind, '0.0.0.0'
 
 get '/' do
-  'hello'
+  erb :index
 end
 
-get '/grid/:id' do
+get '/grid' do
   steamid = Nokogiri::HTML(open("http://steamcommunity.com/id/#{ params[:id] }?xml=1")).css("//profile//steamid64").children
 
   games = JSON.parse(open("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=E1FF6953510EF124ADA4C4E68A11BC35&steamid=#{ steamid }&format=json").read)["response"]["games"]
